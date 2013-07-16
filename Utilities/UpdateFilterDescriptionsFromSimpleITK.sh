3a51d051bf32344aeec7710ac558fc6714900213
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-07-15 21:43:26 -0400

    Updating UpdateFilterDescription script to reference sitk-upstream
    Also cleaned up some reference to the names of repos

--- Utilities/UpdateFilterDescriptionsFromSimpleITK.sh
+++ Utilities/UpdateFilterDescriptionsFromSimpleITK.sh
@@ -14,22 +14,22 @@
 #
 echo "-------------------------------------------------------"
 echo This script will update the SimpleITK filter descriptions
-echo from http://itk.org/SimpleITK.git
+echo in SimpleFilters/Resources/json from
+echo  http://itk.org/SimpleITK.git
 echo "-------------------------------------------------------"
-if [ ! -f UpdateFilterDescriptionsFromSimpleITK.sh ]
+if [ ! -f Utilities/UpdateFilterDescriptionsFromSimpleITK.sh ]
 then
     echo The current working directory $(pwd) is not the top level
-    echo of the SimpleFiltersDescriptions source code tree. Please change
-    echo to the SimpleFiltersDescriptions source directory and re-run 
+    echo of the SlicerSimpleFilters source code tree. Please change
+    echo to the SlicerSimpleFilters source directory and re-run 
     echo this script
     exit 1
 fi
 #
-# Once the merge has been done
-# EDIT THIS SCRIPT to change the hash tag at which to begin the
-# next update...
+# The origin has a branch "sitk-upstream" which contains imported json
+# files as a subtree.
 #
-git branch sitk-upstream UPDATE-HERE
+git branch -f sitk-upstream origin/sitk-upstream
 
 #
 # Make a temp directory to handle the import of the upstream source
@@ -45,7 +45,7 @@ git pull .. sitk-upstream
 # empty out all existing source
 rm -rf *
 #
-# download and copy the necessary double-conversion source
+# download and copy the necessary SimpleITK source
 echo Cloning upstream HEAD from http://itk.org/SimpleITK.git
 echo NOTE: to check out a particular revision for merging
 echo you have to add a git checkout '<hash>'
@@ -55,12 +55,12 @@ git clone git://itk.org/SimpleITK.git sitk
 #
 # recover the upstream commit date.
 cd sitk
+#git checkout  origin/release
 upstream_date="$(git log -n 1 --format='%cd')"
 upstream_hash="$(git rev-parse --short=8 HEAD)"
 cd ..
 
-mkdir -p BasicFilters/json
-cp -r sitk/Code/BasicFilters/json/*.json BasicFilters/json
+find ./sitk/Code/ -name \*.json -exec cp {} ./ \;
 
 # get rid of SimpleITK clone
 rm -rf sitk
@@ -76,7 +76,7 @@ GIT_AUTHOR_DATE="${upstream_date}" \
 git commit -q -m "SimpleITK ${upstream_hash} Filter descriptions (reduced)
 
 This tree was extracted from upstream SimpleITK by the following shell script:
-  ./UpdateFilterDescriptionsFromSimpleITK.sh"
+  ./Utilities/UpdateFilterDescriptionsFromSimpleITK.sh"
 
 #
 # push to the sitk-upstream branch in the
@@ -91,20 +91,13 @@ rm -fr sitk-tmp
 git checkout -b update-sitk master
 #
 # use subtree merge to bring in upstream changes
-git merge -s recursive -X subtree=BasicFilters/json sitk-upstream
-
-# Obtain hash tag at which begin the next update
-sitk_upstream_hash="$(git rev-parse --short=8 sitk-upstream)"
+git merge -s recursive -X subtree=SimpleFilters/Resources/json sitk-upstream
 
 echo "---------------------------------"
 echo 1. If there are conflicts, resolve them and commit.
 echo
 echo "---------------------------------"
-echo 2. Edit line \"git branch sitk-upstream\" in UpdateFilterDescriptionsFromSimpleITK.sh
-echo script with hash tag ${sitk_upstream_hash}.
-echo
-echo Associated commit message should be:
-echo "  Update UpdateFilterDescriptionsFromSimpleITK.sh for ${upstream_hash} snapshot"
+echo 2. Push the updated \"sitk-upstream\" branch to the origin.
 echo
 echo "---------------------------------"
 echo 3. Submit a pull request or directly merge topic update-sitk into master
